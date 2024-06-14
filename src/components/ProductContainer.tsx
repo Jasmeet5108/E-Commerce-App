746b7dc52506f47c46a7ec9bcbcbeaaeb8ebfd89
Jasmeet Singh <jasmeet5108k@gmail.com>
2024-06-14 21:33:38 +0530

    cards-bg

--- src/components/ProductContainer.tsx
+++ src/components/ProductContainer.tsx
@@ -15,10 +15,10 @@ const ProductContainer = () => {
         <>
             <div className='flex flex-wrap items-center gap-2 p-4 mt-4 max-w-screen-2xl mx-auto'>
                 {data && data.map((item, index) => (
-                    <div key={index} className='flex flex-col h-[450px] w-[350px] p-2 gap-5 items-center border border-black rounded-xl'>
-                        <Image className='w-full h-80 border rounded-xl' width={100} height={100} src={item.image} alt="Image" />
+                    <div key={index} className='flex flex-col bg-white h-[450px] w-[350px] p-2 gap-5 items-center border border-black rounded-xl'>
+                        <Image className='w-full h-80 rounded-xl' width={100} height={100} src={item.image} alt="Image" />
                         <p className='text-center text-sm font-semibold line-clamp-3'>{item.title}</p>
-                        <p className='font-semibold'>{item.price}</p>
+                        <p className='font-semibold'>${item.price}</p>
                     </div>
                 ))}
             </div>
